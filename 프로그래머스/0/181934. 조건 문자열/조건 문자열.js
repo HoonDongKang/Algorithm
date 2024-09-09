59c8d2dd0b21fc9b808cada1dd95d551d3acbca8
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2024-09-09 10:07:31 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,9 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === "<") {
+        if(eq === "=") return +(n <= m);
+        return +(n < m)
+    }
+    
+        if(eq === "=") return +(n >= m);
+        return +(n > m)
+}
