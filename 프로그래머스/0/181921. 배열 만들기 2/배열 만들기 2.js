85a51cd9cf5c56bc362c6f92c17e678ae0851e41
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2024-09-11 10:25:28 +0900

    [level 0] Title: 배열 만들기 2, Time: 114.54 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,11 @@
+function solution(l, r) {
+  let result = [];
+  for (let i = l; i <= r; i++) {
+    const str = i.toString();
+    if ([...str].every(char => char === '0' || char === '5')) {
+      result.push(i);
+    }
+  }
+
+  return result.length > 0 ? result : [-1];
+}
