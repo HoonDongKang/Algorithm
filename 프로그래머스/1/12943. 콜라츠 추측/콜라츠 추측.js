de96700dd369ccbc4f7e064d02a48028f2a0032e
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2024-09-25 10:41:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,7 @@
+function solution(num) {
+    for (let answer = 0; answer < 500; answer++) {
+        if (num === 1) return answer;
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    }
+    return -1;
+}
