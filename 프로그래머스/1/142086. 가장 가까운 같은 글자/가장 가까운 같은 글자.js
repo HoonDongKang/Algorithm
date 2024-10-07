5bd827efb2f606dd3b8c838a2ca8796fe2032b28
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2024-10-07 10:15:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.93 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  const result = [];
+  const lastIndex = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (lastIndex[char] !== undefined) {
+      result.push(i - lastIndex[char]);
+    } else {
+      result.push(-1);
+    }
+
+    lastIndex[char] = i;
+  }
+
+  return result;
+}
