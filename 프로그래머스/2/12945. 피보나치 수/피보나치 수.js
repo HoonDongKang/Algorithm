369da54b2eb692904ee4a21d67076486db93fa6d
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2024-10-08 09:43:23 +0900

    [level 2] Title: 피보나치 수, Time: 1.77 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let c = (a + b) % 1234567;
+    a = b;
+    b = c;
+  }
+  return b;
+}
