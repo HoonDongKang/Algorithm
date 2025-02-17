91f359409266fe2151e5eafb6ccc7f11fdc5bf07
Donghoon Kang <d159123@naver.com>
2025-02-18 01:12:26 +0900

    feat: Add recursive function

---
+++ dingcorithm/2nd_week/02_09_factorial.py
@@ -0,0 +1,12 @@
+# factorial(n) = n * factorial(n-1)
+# factorial(n-1) = (n-1) * factorial(n-2)
+
+def factorial(n):
+    # 이 부분을 채워보세요!
+    if n == 1:
+        return 1
+    
+    return n * factorial(n-1)
+
+
+print(factorial(5))
