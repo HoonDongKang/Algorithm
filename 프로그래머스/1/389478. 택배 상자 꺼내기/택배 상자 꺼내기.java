e62d7b87d5168bd1db046d2d8f411c3204c343ed
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2025-03-27 16:02:22 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,43 +1,15 @@
 
 class Solution {
     public int solution(int n, int w, int num) {
-        boolean left = true;
-        int maxC = n/w+(n%w==0?0:1);
-        int c = 0;
-        int r = 0;
-        int numc = 0;
-        int numr = 0;
-        int[][] boxes = new int[maxC][w];
-        for(int i=1;i<=n;i++){
-            if(i==num){
-                numc=c;
-                numr=r;
-            }
-            if(left){
-                boxes[r][c]=i;
-                if(c==w-1){
-                    r++;
-                    left = false;
-                }else{
-                    c++;    
-                }
-            }else{
-                boxes[r][c]=i;
-                if(c==0){
-                    r++;
-                    left = true;
-                }else{
-                    c--;    
-                }
-            }
+        int cnt = 0;
+        while (num <= n) {
+            int targetIndex = (num - 1) % w;
+            int reverseIndex = w - targetIndex - 1;
+            int upperLevelNum = reverseIndex * 2 + 1;
+            num += upperLevelNum;
+            cnt++;
         }
-        int answer = 0;
-        while(numr<maxC){
-            if(boxes[numr][numc]!=0)
-                answer++;
-            numr++;
-        }
-        
-        return answer;
+
+        return cnt;
     }
 }
