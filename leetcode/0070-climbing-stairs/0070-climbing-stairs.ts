861d9a5132de7650252e0e3c08bb8083fd68543d
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2025-04-08 16:50:46 +0900

    Time: 0 ms (100%), Space: 55.2 MB (54.66%) - LeetHub

--- leetcode/0070-climbing-stairs/0070-climbing-stairs.ts
+++ leetcode/0070-climbing-stairs/0070-climbing-stairs.ts
@@ -1,13 +1,17 @@
 function climbStairs(n: number): number {
     if (n <= 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 2;
 
-    let dpArr: number[] = new Array(n);
-    dpArr[0] = 1;
-    dpArr[1] = 2;
+    let prev1 = 1;
+    let prev2 = 2;
 
-    for (let i = 2; i < n; i++) {
-        dpArr[i] = dpArr[i - 1] + dpArr[i - 2];
+    for (let i = 3; i < n + 1; i++) {
+        let currentValue = prev1 + prev2;
+
+        prev1 = prev2;
+        prev2 = currentValue;
     }
 
-    return dpArr[n - 1];
+    return prev2;
 };
