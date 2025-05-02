a2b79b8449ed456d44499ac7b648095d69d0b070
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2025-05-02 13:41:05 +0900

    Time: 13 ms (11.5%), Space: 58.2 MB (59.66%) - LeetHub

---
+++ leetcode/0139-word-break/0139-word-break.ts
@@ -0,0 +1,16 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+    const wordSet = new Set(wordDict);
+    const dp = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for (let end = 1; end <= s.length; end++) {
+        for (let start = 0; start < end; start++) {
+            const isExists = wordSet.has(s.slice(start, end));
+            if (isExists && dp[start]) {
+                dp[end] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.length];
+};
