c644b0e27e118834d50d1ef87582e23b44be564b
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2025-06-25 10:53:27 +0900

    Time: 2 ms (21.48%), Space: 61.7 MB (53.52%) - LeetHub

---
+++ LeetCode/Medium/0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+    const values: number[] = [];
+
+    function dfs(node: TreeNode | null) {
+        if (!node) return;
+        dfs(node.left);
+        values.push(node.val);
+        dfs(node.right);
+    }
+
+    dfs(root);
+
+    return values[k - 1];
+};
