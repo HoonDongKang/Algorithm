0836c4a8b5646f6e52b8f5cee01b1e227ba632e1
Donghoon Kang <102861831+HoonDongKang@users.noreply.github.com>
2025-07-08 10:53:50 +0900

    Time: 3 ms (97.03%), Space: 62.4 MB (86.5%) - LeetHub

---
+++ LeetCode/Easy/0572-subtree-of-another-tree/0572-subtree-of-another-tree.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    const pre = (node: TreeNode | null) => {
+        if (!node) return "N";
+        return `(${node.val},${pre(node.left)},${pre(node.right)})`;
+    };
+
+    return pre(root).includes(pre(subRoot));
+};
