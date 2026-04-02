38ca83af2eb0eedd7aaa76912510c6f8e3e970f1
Donghoon Kang <d159123@naver.com>
2026-04-03 00:14:45 +0900

    [Bronze V] Title: 개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,16 @@
+import sys
+
+def sys_input():
+    return sys.stdin.readline().rstrip()
+
+def solve(inputs:list[int]) -> list[int]:
+    pass
+def main():
+    print("|\\_/|")
+    print("|q p|   /}")
+    print("( 0 )\"\"\"\\")
+    print("|\"^\"`    |")
+    print("||_/=\\\\__|")
+
+if __name__ == "__main__":
+    main()
