99918849797516ef292e01e7334b3a1b398481aa
Donghoon Kang <d159123@naver.com>
2026-04-28 15:02:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.py
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,12 +1,9 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        sorted_num = sorted(nums)
+        set_num = set(nums)
         small_num = 1
 
-        for num in sorted_num:
-            if num <= 0:
-                continue
-            else:
-                if num <= small_num:
-                    small_num = num + 1
-        return small_num
+        while True:
+            if small_num not in set_num:
+                return small_num
+            small_num += 1
