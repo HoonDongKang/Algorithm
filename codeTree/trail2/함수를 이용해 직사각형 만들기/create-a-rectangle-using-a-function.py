acfa99ea9cb5a56fc1937615feb97043f30358e3
Donghoon Kang <d159123@naver.com>
2026-05-22 10:45:03 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (70ms, 17MB)

---
+++ codeTree/trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def printSquare(n:int, m:int):
+    for i in range(n):
+        for j in range(m):
+            print(1, end="")
+        print("")
+
+printSquare(n,m)
