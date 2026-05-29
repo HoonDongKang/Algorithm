5c22d9ecc3bf1ff25d35adae377ab2866c96612d
Donghoon Kang <d159123@naver.com>
2026-05-29 14:51:02 +0900

    [Passed] 정수 명령 처리 (184ms, 24MB)

---
+++ codeTree/trail3/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,20 @@
+N = int(input())
+command = []
+value = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push":
+        value.append(int(line[1]))
+    elif line[0] == 'pop':
+        print(value.pop())
+    elif line[0] == 'size':
+        print(len(value))
+    elif line[0] == 'empty':
+        print(0 if len(value) else 1)
+    elif line[0] == 'top':
+        print(value[len(value) - 1])
+    
+
+# Please write your code here.
